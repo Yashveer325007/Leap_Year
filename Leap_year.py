1fef950807dd87833cd3f574bdb9cbf77604040e
Yashveer Singh Makhloga <119954921+Yashveer325007@users.noreply.github.com>
2023-10-29 19:04:30 +0530

    Update Leap_year.py

--- Leap_year.py
+++ Leap_year.py
@@ -1,1 +1,12 @@
+year=int(input("Enter the year"))
+if(year%4==0):
+  if(year%100!=0):
+    print("Leap year")
+  else:
+    if(year%400==0):
+      print("Leap year")
+    else:
+      print("Not leap year")
+else:
+  print("Not leap year")
 
